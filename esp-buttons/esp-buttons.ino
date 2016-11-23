273ac8e4efad77bd2a97575c1358842298cc1a75
Alin Micu <micu.alin.ctin@gmail.com>
2016-11-23 02:43:53 +0200

    Fix debouncing issues. Remove commented-out code.

--- esp-buttons/esp-buttons.ino
+++ esp-buttons/esp-buttons.ino
@@ -4,13 +4,7 @@
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"          // https://github.com/tzapu/WiFiManager
 
-void configModeCallback (WiFiManager *myWiFiManager)
-{
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-  //if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-}
+//#define SIMPLE_DEBOUNCE 1
 
 const uint8_t BUTTON1 = 16;
 const uint8_t BUTTON2 = 14;
@@ -27,27 +21,33 @@ uint8_t pressed[BUTTONS_NUM] = {0};
 uint8_t justpressed[BUTTONS_NUM] = {0};
 uint8_t justreleased[BUTTONS_NUM] = {0};
 
+#ifdef SIMPLE_DEBOUNCE
 const uint32_t BUTTON_DEBOUNCE_TIME = 200;
 const uint32_t POLL_BUTTON_TIME = 600;
+#else
+const uint32_t BUTTON_DEBOUNCE_TIME = 100;
+#endif
 const uint32_t REQUEST_TIME = 30000;
-
-// const char* serverButtonsEndpoint = "http://esp-buttons.herokuapp.com/buttons";
+const uint16_t HTTP_REQ_TIMEOUT = 5000;
 const char* serverButtonsEndpoint = "http://mirtes99.ddns.net:1001/record/post";
 
 HTTPClient http;
-uint16_t buttonsPresses[BUTTONS_NUM] = {0};
+uint32_t buttonsPresses[BUTTONS_NUM] = {0};
 unsigned long lastTime = 0;
 unsigned long lastTimeRequest = 0;
 unsigned long lastTimeDebounce = 0;
 
+void configModeCallback (WiFiManager *myWiFiManager)
+{
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
 void setup()
 {
-    // setup GPIO
-//  pinMode(BUTTON1, INPUT);
-//  pinMode(BUTTON2, INPUT);
-//  pinMode(BUTTON3, INPUT);
-//  pinMode(BUTTON4, INPUT);
-//  pinMode(BUTTON5, INPUT);
+  // setup GPIO
   pinMode(STATUS_LED, OUTPUT);
   digitalWrite(STATUS_LED, LOW);
   for (size_t i = 0; i < BUTTONS_NUM; ++i) {
@@ -59,27 +59,27 @@ void setup()
   Serial1.begin(115200);
   Serial.begin(115200);
   
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
+  // WiFiManager
+  // Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
-  //reset settings - for testing
+  // reset settings - for testing
 //  wifiManager.resetSettings();
 
-  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  // set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
   wifiManager.setAPCallback(configModeCallback);
 
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
+  // fetches ssid and pass and tries to connect
+  // if it does not connect it starts an access point with the specified name
+  // here  "AutoConnectAP"
+  // and goes into a blocking loop awaiting configuration
   if(!wifiManager.autoConnect()) {
     Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
+    // reset and try again, or maybe put it to deep sleep
     ESP.reset();
     delay(1000);
   } 
 
-  //if you get here you have connected to the WiFi
+  // if you get here you have connected to the WiFi
   Serial.println("Connected to WiFi");
 
   uint8_t macAddr[6] = {0};  
@@ -93,10 +93,10 @@ void setup()
 
 void checkButtons()
 {
-  static byte previousstate[BUTTONS_NUM];
-  static byte currentstate[BUTTONS_NUM];
+  static uint8_t previousstate[BUTTONS_NUM];
+  static uint8_t currentstate[BUTTONS_NUM];
   static unsigned long lasttime = 0;
-  byte index = 0;
+  size_t index = 0;
 
   if (millis() < lasttime) {
      lasttime = millis(); // we wrapped around, lets just try again
@@ -109,54 +109,60 @@ void checkButtons()
   // ok we have waited BUTTON_DEBOUNCE_TIME milliseconds, lets reset the timer
   lasttime = millis();
   
-  for (index = 0; index < BUTTONS_NUM; index++) {
+  for (index = 0; index < BUTTONS_NUM; ++index) {
     justreleased[index] = 0;  // when we start, we clear out the "just" indicators
     justpressed[index] = 0;
-     
     currentstate[index] = !digitalRead(BUTTONS[index]);   // read the button
-    
-//    Serial.print(index, DEC);
-//    Serial.print(": cstate=");
-//    Serial.print(currentstate[index], DEC);
-//    Serial.print(", pstate=");
-//    Serial.print(previousstate[index], DEC);
-//    Serial.print(", press=");
-    
     if (currentstate[index] == previousstate[index]) {
-      if ((pressed[index] == BTN_PRESSED) && (currentstate[index] == BTN_PRESSED)) {
+      if ((pressed[index] == LOW) && (currentstate[index] == LOW)) {
           // just pressed
           justpressed[index] = 1;
       }
-      else if ((pressed[index] == BTN_NOT_PRESSED) && (currentstate[index] == BTN_NOT_PRESSED)) {
+      else if ((pressed[index] == HIGH) && (currentstate[index] == HIGH)) {
           // just released
           justreleased[index] = 1;
       }
       pressed[index] = !currentstate[index];
     }
-//    Serial.println(pressed[index], DEC);
     previousstate[index] = currentstate[index];   // keep a running tally of the buttons
   }
+}
+
+uint8_t thisButtonJustPressed()
+{
+  static uint8_t previous_keystate[BUTTONS_NUM] = {0};
+  static uint8_t current_keystate[BUTTONS_NUM] = {0};
+  uint8_t thisButton = 255;
+  checkButtons();  // check the buttons; get the current state
+  for (size_t i = 0; i < BUTTONS_NUM; ++i) {
+    current_keystate[i] = justpressed[i];
+    if (current_keystate[i] != previous_keystate[i]) {
+      if (current_keystate[i]) {
+        thisButton = i;
+      }
+    }
+    previous_keystate[i]=current_keystate[i];
+  }  
+  return thisButton;
 }
 
 void loop()
 {
   bool clearButtons = false;
 
-//  checkButtons();
+#ifdef SIMPLE_DEBOUNCE
   if(millis() < lastTimeDebounce) {
     lastTimeDebounce = millis();
   }
   if (millis() >= (lastTime + BUTTON_DEBOUNCE_TIME)) {
     for (size_t i = 0; i < BUTTONS_NUM; ++i) {
       if (digitalRead(BUTTONS[i]) == HIGH) {
-//        Serial.printf("BTN%u\n", i + 1);
         justpressed[i] = 1;
         break;
       }
     }
     lastTimeDebounce = millis();
   }
-  
 
   if (millis() < lastTime) {
     lastTime = millis();
@@ -167,12 +173,17 @@ void loop()
         Serial.printf("BTN%u\n", i + 1);
         ++buttonsPresses[i];
       }
-//      Serial.printf("BTN presses: %u\t%u\t%u\t%u\t%u\n", buttonsPresses[0], buttonsPresses[1], buttonsPresses[2], buttonsPresses[3], buttonsPresses[4]);
       justpressed[i] = 0;
     }
     lastTime = millis();    
   }
-  
+#else
+  uint8_t thisButton = thisButtonJustPressed();
+  if (thisButton < BUTTONS_NUM) {
+    ++buttonsPresses[thisButton];
+    Serial.printf("btn%u\n", thisButton + 1);
+  }
+#endif
 
   if (millis() < lastTimeRequest) {
     lastTimeRequest = millis();
@@ -191,7 +202,8 @@ void loop()
       }
       Serial.println("REQUEST BODY:");
       Serial.println(httpButtonsRequest);
-    
+
+      http.setTimeout(HTTP_REQ_TIMEOUT);
       http.begin(serverButtonsEndpoint);
       http.addHeader("Content-Type", "text/plain");
       http.addHeader("Connection", "close");
@@ -223,3 +235,4 @@ void loop()
   }
   
 }
+
